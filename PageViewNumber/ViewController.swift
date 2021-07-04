2dab9be17b7896b2f4b490c662e40f675260fa7a
davidyoon <qqpo12@naver.com>
2021-07-04 12:53:17 +0900

    add pageControlView with number list

--- PageViewNumber/ViewController.swift
+++ PageViewNumber/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblNumber: UILabel!
+    @IBOutlet var pageView: UIPageControl!
+    
+    let numberList = [1,2,3,4,5,6,7,8,9,10]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pageView.numberOfPages = numberList.count
+        pageView.currentPage = 0
+        pageView.pageIndicatorTintColor = UIColor.green
+        pageView.currentPageIndicatorTintColor = UIColor.red
+        lblNumber.text = String(numberList[0])
+        
     }
 
-
+    @IBAction func pageChange(_ sender: UIPageControl) {
+        lblNumber.text = String(numberList[pageView.currentPage])
+    }
+    
 }
 
